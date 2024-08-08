716a33bc718b4a9c7d0006f8afac52dd73006420
shahzadakram786 <alyakram786@gmail.com>
2024-08-09 00:15:23 +0500

    .

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            hello there i am shahzad akram
+            hello there i am shahzad akram
+            hello there i am shahzad akram
+            hello there i am shahzad akram
+
+        </div>
+    )
+}
+
+export default Home

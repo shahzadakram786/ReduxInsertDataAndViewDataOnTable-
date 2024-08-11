ec349d6adae7bdcb81dc5de31f7bc7d6366397d9
shahzadakram786 <alyakram786@gmail.com>
2024-08-11 15:58:16 +0500

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,98 @@ import "./App.css";
 import Home from "./Home";
 
 function App() {
-  return <Home />;
+    return <Home / > ;
 }
 
 export default App;
+
+
+
+
+
+john azhar
+day 1 st
+water 300
+lunch 2500
+tea 400
+dinner 3250
+
+day 2
+water 300
+jeep 16000
+lunch at tatoo valley 3200
+porter 2000
+
+day 3
+porter 1000
+water 500
+tea at view point 1000
+
+day 4
+guide to base camp 1000
+tea and refrehment 1350
+water 550
+
+
+day 5
+tea 750
+water 300
+
+day 6
+porter 1000
+tea and water 1200
+lunch 4200
+drinks 750
+room 2000
+
+day 7
+breakfast 610
+lunch 1200
+romm  2000
+total
+
+
+
+
+
+
+
+
+
+
+tara to sadiq
+DAY 1
+ROOM 2000
+water 350
+lunch 4100
+DRINKS 750
+
+DAY 2
+FORT 4500
+TEA AND WALNAT CAKE 3100
+baltit fort 4500
+water 350
+fuel 13500
+lunch yak grill 7500
+fruit 750
+
+day 3
+tae with walnut cake 1700
+lunch 7500
+water 300
+driver 6000
+
+day 4
+fuel 13000
+fruits 750
+lunch 3880
+tea 500
+water and drinks 1250
+fuel 5000
+dinner 570
+
+day 5
+fuel 12000
+water and tea 800
+lunch 3510
+toll  plaza  100
